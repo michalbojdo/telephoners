44ab9cfa9d5d38b95909baa2dbd3333385e6981a
Michal Bojdo <michal.bojdo.kontakt@gmail.com>
2025-12-20 02:51:33 +0100

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<script setup></script>
+
+<template>
+  <header>
+    
+    <h2>
+      <l></l>
+      <div>
+        SKN
+        <img src="@/assets/logos/logo_light_lb.png" />
+        Telephoners
+      </div>
+      <div class="icons social-media">        
+        <img src="@/assets/logos/logo_light.png" />
+        <img src="@/assets/social-media/linkedin.png" />
+        <img src="@/assets/social-media/facebook.png" />
+        <img src="@/assets/social-media/youtube.png" />
+      </div>
+    </h2>
+  </header>
+</template>
+
+<style>
+header {
+  padding: 1rem;
+
+  h2 {
+    /* display: flex;
+    justify-content: space-between; */ 
+    display:grid;
+    grid-template-columns: 1fr 1fr 1fr; 
+    color: #fff;
+    font-weight: 300;
+    height: 3rem;
+
+    * {
+      display:flex;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+      max-height: 3rem;
+      vertical-align: middle;
+    }
+  }
+}
+.icons.social-media {
+  display: flex;
+  justify-content: flex-end; 
+  * {
+    height: 2rem;
+    margin: 5px;
+  }
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+  <Header />
+  <div class="background-image" />  
+  <div class="background-tint" />
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+  .background-tint{
+    background-color: var(--tele-black);
+     background-position: center;
+  background-size: 80vh;
+  filter: opacity(0.5);
+  position: fixed;
+  display: block;
+  z-index: -1;
+  width: 100vw;
+  height: 100vh;
+  }
+.background-image {
+  background-image: url("@/assets/drone.webp");
+  background-position:center;
+  background-size:cover;
+  filter: opacity(0.8);
+  position: fixed;
+  display: block;
+  z-index: -1;
+  width: 100vw;
+  height: 100vh;
+}
+main {
+  padding: 0px 20px;
+}
+</style>
